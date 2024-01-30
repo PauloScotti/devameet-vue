51a3fc0ff7470d6ee7b98861b218981d3c2474cb
PauloScotti <44655524+PauloScotti@users.noreply.github.com>
2024-01-30 16:56:28 -0300

    aula 03 parte 01

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import HeaderVue from "../components/general/Header.vue";
+import { defineComponent } from "vue";
+
+    export default defineComponent({
+    components: { HeaderVue }
+});
+</script>
+
+<template>
+    <HeaderVue />
+</template>
+
+<style lang="scss"></style>
