189472f3b56ff941b0d07e52b311291bb8e823c3
PauloScotti <44655524+PauloScotti@users.noreply.github.com>
2024-01-30 09:59:29 -0300

    aula 02 parte 01
    Assets, styles globais

--- src/views/LoginVue.vue
+++ src/views/LoginVue.vue
@@ -8,4 +8,13 @@
 
 <template>
     <h1>Login</h1>
+    <div class="container-public">
+        <div class="logo">
+            <img src="../assets/images/logo.svg" alt="Logo Devameet">
+        </div>
+        <form>
+            <input type="email" name="E-mail" id="email" placeholder="E-mail">
+            <input type="password" name="Senha" id="password" placeholder="Senha">
+        </form>
+    </div>
 </template>
