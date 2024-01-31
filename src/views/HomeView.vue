60f20ad6ea5721bbe5eb8fbb87c7bfa3d4d7dadf
PauloScotti <44655524+PauloScotti@users.noreply.github.com>
2024-01-31 14:05:46 -0300

    aula 03 parte 03
    Container principal e ajuste de estilos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
     import HeaderVue from "../components/general/Header.vue";
+    import MeetHomeVue from "../components/meet/MeetHome.vue";
     import FooterVue from "../components/general/Footer.vue";
     import { defineComponent } from "vue";
 
     export default defineComponent({
-    components: { HeaderVue, FooterVue }
+    components: { HeaderVue, MeetHomeVue, FooterVue }
 });
 </script>
 
 <template>
     <HeaderVue />
+    <MeetHomeVue />
     <FooterVue />
 </template>
 
