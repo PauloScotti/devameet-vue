9f63ac09f0cf6f777532e2b54f7b8e12558d7e37
PauloScotti <44655524+PauloScotti@users.noreply.github.com>
2024-02-10 15:44:22 -0300

    aula 07 parte 02

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import { useAccessTokenStore } from '@/stores/accessToken';
 import ProfileViewVue from '@/views/ProfileView.vue';
 import MeetAddViewVue from '@/views/MeetAddView.vue';
 import MeetEditViewVue from '@/views/MeetEditView.vue';
+import RoomLinkViewVue from '@/views/RoomLinkView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,12 @@ const router = createRouter({
       path: '/add',
       name: 'add',
       component: MeetAddViewVue,
+      meta: {rotaPrivada: true}
+    },
+    {
+      path: '/link',
+      name: 'link',
+      component: RoomLinkViewVue,
       meta: {rotaPrivada: true}
     },
     {
---
+++ src/views/RoomLinkView.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import HeaderVue from "../components/general/Header.vue";
+    import FooterVue from "../components/general/Footer.vue";
+    import RouterLink from "../components/room/RoomLink.vue";
+    import { defineComponent } from "vue";
+    import router from "@/router";
+
+    export default defineComponent({
+        setup(){
+            return {
+                mobile: window.innerWidth <= 992
+            }
+        },
+        beforeCreate(){
+            if(!this.mobile) {
+                router.push({name: 'home'});
+            }
+        },
+        components: { HeaderVue, FooterVue, RouterLink }
+    });
+</script>
+
+<template>
+    <HeaderVue />
+    <RouterLink />
+    <FooterVue />
+</template>
+
+<style lang="scss"></style>
